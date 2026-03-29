b5c9d7467372075de777497a806c5869f318ab4d
daankuiper06-hue <daankuiper06@gmail.com>
2026-03-30 00:17:37 +0200

    Update route.js

--- app/api/labor/route.js
+++ app/api/labor/route.js
@@ -1,1 +1,20 @@
+import { pool } from "../../../../lib/db";
 
+export async function POST(req) {
+  const body = await req.json();
+
+  const res = await pool.query(
+    `INSERT INTO labor (project_id, medewerker, datum, uren, tarief)
+     VALUES ($1, $2, $3, $4, $5)
+     RETURNING *`,
+    [
+      body.project_id,
+      body.medewerker || "",
+      body.datum || null,
+      body.uren ?? 0,
+      body.tarief ?? 25,
+    ]
+  );
+
+  return Response.json(res.rows[0]);
+}
