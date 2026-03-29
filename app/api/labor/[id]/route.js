1920e2bfaf1d0c9473df1176c68ecfc037b0bd90
daankuiper06-hue <daankuiper06@gmail.com>
2026-03-30 00:18:07 +0200

    Update route.js

--- app/api/labor/[id]/route.js
+++ app/api/labor/[id]/route.js
@@ -1,1 +1,25 @@
+import { pool } from "../../../../../lib/db";
 
+export async function PUT(req, { params }) {
+  const id = Number(params.id);
+  const body = await req.json();
+
+  const res = await pool.query(
+    `UPDATE labor
+     SET medewerker = $1,
+         datum = $2,
+         uren = $3,
+         tarief = $4
+     WHERE id = $5
+     RETURNING *`,
+    [body.medewerker, body.datum || null, body.uren, body.tarief, id]
+  );
+
+  return Response.json(res.rows[0]);
+}
+
+export async function DELETE(_, { params }) {
+  const id = Number(params.id);
+  await pool.query("DELETE FROM labor WHERE id = $1", [id]);
+  return Response.json({ ok: true });
+}
