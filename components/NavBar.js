4c293e38ecc1e68f8487cb6462c054d435aeb912
daankuiper06-hue <daankuiper06@gmail.com>
2026-03-29 23:50:41 +0200

    Add NavBar component with navigation links

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,1 +1,19 @@
+export default function NavBar() {
+  return (
+    <nav style={nav}>
+      <a href="/">Overzicht</a>
+      <a href="/project">Projecten</a>
+      <a href="/calculatie">Calculatie</a>
+      <a href="/factuur">Factuur</a>
+    </nav>
+  );
+}
 
+const nav = {
+  display: "flex",
+  gap: "16px",
+  background: "#fff",
+  padding: "12px",
+  borderRadius: "12px",
+  marginBottom: "20px",
+};
