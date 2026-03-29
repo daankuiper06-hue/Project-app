76cf417cf6d9443056f2e3fe675a261943791490
daankuiper06-hue <daankuiper06@gmail.com>
2026-03-30 00:16:47 +0200

    Update route.js

--- app/api/materials/route.js
+++ app/api/materials/route.js
@@ -1,1 +1,20 @@
+import { pool } from "../../../../lib/db";
 
+export async function POST(req) {
+  const body = await req.json();
+
+  const res = await pool.query(
+    `INSERT INTO materials (project_id, naam, aantal, inkoopprijs, btw)
+     VALUES ($1, $2, $3, $4, $5)
+     RETURNING *`,
+    [
+      body.project_id,
+      body.naam || "",
+      body.aantal ?? 1,
+      body.inkoopprijs ?? 0,
+      body.btw ?? 21,
+    ]
+  );
+
+  return Response.json(res.rows[0]);
+}
