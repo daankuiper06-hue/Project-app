4dcf823146b76e0decc0d73c6d21f10e6162aeb3
daankuiper06-hue <daankuiper06@gmail.com>
2026-03-30 00:16:10 +0200

    Update route.js

--- app/api/projects/[id]/route.js
+++ app/api/projects/[id]/route.js
@@ -1,1 +1,33 @@
+import { pool } from "../../../../../lib/db";
 
+export async function PUT(req, { params }) {
+  const id = Number(params.id);
+  const body = await req.json();
+
+  const res = await pool.query(
+    `UPDATE projects
+     SET naam = $1,
+         klant = $2,
+         datum = $3,
+         status = $4,
+         marge_percentage = $5
+     WHERE id = $6
+     RETURNING *`,
+    [
+      body.naam,
+      body.klant,
+      body.datum,
+      body.status,
+      body.marge_percentage ?? 20,
+      id,
+    ]
+  );
+
+  return Response.json(res.rows[0]);
+}
+
+export async function DELETE(_, { params }) {
+  const id = Number(params.id);
+  await pool.query("DELETE FROM projects WHERE id = $1", [id]);
+  return Response.json({ ok: true });
+}
